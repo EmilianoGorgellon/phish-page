d353d317c67503d1454acdc1f3365d645d1d424c
Emiliano Gorgellon <EGGorgellon@teco.com.ar>
2022-04-01 17:46:09 -0300

    Finish with comments

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 import data from "./data.js";
-let buttonNext = document.getElementById("next");
-let buttonPrev = document.getElementById("prev");
-let buttonPlay = document.querySelector(".button--link");
-let containerMain = document.querySelector(".container--all");
-let getAllPages = document.querySelectorAll(".pages");
-
+const buttonBack = document.getElementById("prev");
+const buttonPlay = document.querySelector(".button--play");
+const containerMain = document.querySelector(".container--all");
+const getAllPages = document.querySelectorAll(".pages");
 let position = 0;
-containerMain.style.width = `${(getAllPages.length + 1) * 100}%`
+let result = 0;
+let questions = 0;
+let button_replay;
+containerMain.style.width = `${(getAllPages.length + 2) * 100}%`
 
+const sendResults = () => {
+    let text_answer = document.querySelector(".result-page");
+    // Lo deje comentado por si el resultado es > 5 de las letras en verde o en rojo
+    // result > 5 ? text_answer.innerHTML = 
+    //     `
+    //         <h5 class="title-result">Game Over</h5>
+    //         <p class="result result-correct">You got ${result}/${data.length} correct!</p>
+    //         <button class="button-default button-replay">Play Again</button>
+    //     ` 
+    //     : 
+    //     `
+    //         <h5 class="title-result">Game Over</h5>
+    //         <p class="result result-incorrect">You got ${result}/${data.length} correct!</p>
+    //         <button class="button-default button-replay">Play Again</button>
+    //     `
+    text_answer.innerHTML = 
+        `
+            <h5 class="title-result">Game Over</h5>
+            <p class="result result-correct">You got ${result}/${data.length} correct!</p>
+            <button class="button-default button-replay">Play Again</button> 
+        `;
+    button_replay =  document.querySelector(".button-replay");
+    button_replay.addEventListener('click', () => window.location.reload())
+
+}
 const next = () => {
-    if (position !== -(getAllPages.length * 10)) {
-        position = position - 10;
-        containerMain.style.transform = `translateX(${position}%)`;
-    } else {
-        containerMain.style.transform = `translateX(${position - 1}%)`;
-        pageBack();
-    }
+    position = position - 8.33;
+    containerMain.style.transform = `translateX(${position}%)`;
+    if (questions === data.length) return sendResults();
 }
 
 const prev = () => {
-    if (position !== 0) {
-        position = position + 10;
+    if (position < -9) {
+        position = position + 8.33;
         containerMain.style.transform = `translateX(${position}%)`
     } else {
         containerMain.style.transform = `translateX(${position + 1}%)`;
@@ -28,43 +50,53 @@ const prev = () => {
     }
 }
 
-buttonNext.addEventListener('click',next);
-buttonPlay.addEventListener('click', next);
-buttonPrev.addEventListener('click', prev);
+buttonPlay.addEventListener('click', () => {
+    next();
+    buttonBack.classList.remove('no-show')
+});
+buttonBack.addEventListener('click', prev);
 
 const pageBack = () => setTimeout(() => {
     containerMain.style.transform = `translateX(${position}%)`;
 }, 300)
 
 getAllPages.forEach((element, i) => {
-    console.log(i)
     element.insertAdjacentHTML(`beforeend`,
         `<div class="container--data-questions">
             <img class="data--img" src="${data[i].imageURL}" alt="image-${i}" />
             <div class="container--buttons-question">
-                <button class="button-phish button" dataId=${i} state=${false}>Phish</button>
-                <button class="button-legit button" dataId=${i} state=${true}>Legit</button>
+                <button class="button-phish button-question button-default" dataId=${i} state=${false}>Phish</button>
+                <button class="button-legit button-question button-default" dataId=${i} state=${true}>Legit</button>
+            </div>
+            <div class="container--next-answer no-show">
+                <button class="button-next button-default">Next <i class="fas fa-arrow-right"></i></button>
             </div>
             <div class="container--data-text no-show">
-                <p>${data[i].tips[0]}</p>
-                <p>${data[i].tips[1]}</p>
-                <p>${data[i].tips[2]}</p>
+                <h4 class="data--text-title">Tips</h4>
+                <p class="data--text-content">${data[i].tips}</p>
             </div>
         </div>`
     )
 });
 
-
-let buttons = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button-question");
+const container_button_answer = document.querySelectorAll(".container--next-answer")
 buttons.forEach(element => {
     element.addEventListener('click', (e) => {
         let { dataId, state } = e.target.attributes;
-        console.log(data[dataId.value].answer)
-        console.log(state.value)
+        let content_text;
+        questions++;
         if (`${data[dataId.value].answer}` === `${state.value}`) {
-            console.log("esta bien")
+            result++;
+            content_text = "<h3 class='state-true'>Correct, it's legit!</h3>";
+        } else {
+            content_text = "<h3 class='state-false'>Incorrect, it's a phish!</h3>";
         }
-        next()
+        container_button_answer[dataId.value].insertAdjacentHTML('afterbegin', content_text);
+        container_button_answer[dataId.value].classList.remove('no-show');
+        document.querySelectorAll(".container--data-text")[dataId.value].classList.remove('no-show');
+        document.querySelectorAll(".container--buttons-question")[dataId.value].classList.add("no-show");
     })
 })
-console.log(buttons)
+const button_next = document.querySelectorAll(".button-next");
+button_next.forEach(element => element.addEventListener('click', next))
